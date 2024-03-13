c8750d2de4f51fbda8b8045ee3458c4822427465
hieunqdev <hieunqdev@gmail.com>
2024-03-13 17:58:46 +0700

    INIT: Database connection.

---
+++ BeautyEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeautyEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
