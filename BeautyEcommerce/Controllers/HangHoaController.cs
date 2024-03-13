788be2b5a304a69857b4423b474e1bfce3d32754
hieunqdev <hieunqdev@gmail.com>
2024-03-13 22:23:36 +0700

    PRODUCT_PAGE: Design layout product page, Filter category.

---
+++ BeautyEcommerce/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyEcommerce.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index(int? loai)
+        {
+            return View();
+        }
+    }
+}
