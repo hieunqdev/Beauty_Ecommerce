4032fb9529d1ba6566d72a6b287987c00a437511
hieunqdev <hieunqdev@gmail.com>
2024-03-16 16:50:46 +0700

    AUTHENTICATION: Login, logout for customer user.

--- BeautyEcommerce/Controllers/KhachHangController.cs
+++ BeautyEcommerce/Controllers/KhachHangController.cs
@@ -1,7 +1,11 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
 using BeautyEcommerce.Data;
 using BeautyEcommerce.Helpers;
 using BeautyEcommerce.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BeautyEcommerce.Controllers
@@ -16,6 +20,7 @@ namespace BeautyEcommerce.Controllers
             this._context = context;
             this._mapper = mapper;
         }
+        #region Dang ky
         [HttpGet]
         public IActionResult DangKy()
         {
@@ -46,6 +51,78 @@ namespace BeautyEcommerce.Controllers
                 }
             }
             return View();
+        }
+        #endregion
+
+        #region Dang nhap
+        [HttpGet]
+        public IActionResult DangNhap(string? ReturnUrl) 
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> DangNhapAsync(LoginVM model, string? ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            if (ModelState.IsValid)
+            {
+                var khachHang = _context.KhachHangs.SingleOrDefault(user => user.MaKh == model.UserName);
+                if (khachHang == null)
+                {
+                    ModelState.AddModelError("lỗi", "Không tồn tại");
+                } else
+                {
+                    if (!khachHang.HieuLuc)
+                    {
+                        ModelState.AddModelError("lỗi", "Tài khoản bị khóa");
+                    }
+                    else
+                    {
+                        if (khachHang.MatKhau != model.Password.ToMd5Hash(khachHang.RandomKey))
+                        {
+                            ModelState.AddModelError("lỗi", "Sai thông tin đăng nhập");
+                        }
+                        else
+                        {
+                            var claims = new List<Claim>() {
+                                new Claim(ClaimTypes.Email, khachHang.Email),
+                                new Claim(ClaimTypes.Name, khachHang.HoTen),
+                                new Claim("CustomerId", khachHang.MaKh),
+                                new Claim(ClaimTypes.Role, "Customer")
+                            };
+
+                            var claimsIdentity = new ClaimsIdentity(
+                                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                            await HttpContext.SignInAsync(claimsPrincipal);
+                            if (Url.IsLocalUrl(ReturnUrl)) 
+                            { 
+                                return Redirect(ReturnUrl);
+                            } else
+                            {
+                                return Redirect("/");
+                            }
+                        }
+                    }
+                }
+            }
+            return View();
+        }
+        #endregion
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();  
+        }
+
+        [Authorize]
+        public async Task<IActionResult> DangXuat()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
         }
     }
 }
