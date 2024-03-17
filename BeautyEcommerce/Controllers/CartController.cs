9061f9865ac4a6862bd2f91656aae989a61ca1de
hieunqdev <hieunqdev@gmail.com>
2024-03-17 18:31:18 +0700

    PAYMENT: Design payment page. Update payment method cod.

--- BeautyEcommerce/Controllers/CartController.cs
+++ BeautyEcommerce/Controllers/CartController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using BeautyEcommerce.Helpers;
 using BeautyEcommerce.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace BeautyEcommerce.Controllers
 {
@@ -57,6 +59,74 @@ namespace BeautyEcommerce.Controllers
                 HttpContext.Session.Set(MySetting.CART_KEY, gioHang);
             }
             return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Checkout() { 
+            if (Cart.Count == 0)
+            {
+                return RedirectToAction("/");
+            }
+
+            return View(Cart);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Checkout(CheckoutVM model)
+        {
+            var customerId = HttpContext.User.Claims.SingleOrDefault(p => p.Type == MySetting.CLAIM_CUSTOMER_ID).Value;
+            var khachHang = new KhachHang();
+            if (model.GiongKhachHang)
+            {
+                khachHang = _context.KhachHangs.SingleOrDefault(kh => kh.MaKh == customerId);
+            }
+
+            var hoadon = new HoaDon
+            {
+                MaKh = customerId,
+                HoTen = model.HoTen ?? khachHang.HoTen,
+                DiaChi = model.DiaChi ?? khachHang.DiaChi,
+                DienThoai = model.DienThoai ?? khachHang.DienThoai,
+                NgayDat = DateTime.Now,
+                CachThanhToan = "COD",
+                CachVanChuyen = "GRAB",
+                MaTrangThai = 0,
+                GhiChu = model.GhiChu
+            };
+
+            _context.Database.BeginTransaction();
+            try
+            {
+                _context.Database.CommitTransaction();
+                _context.Add(hoadon);
+                _context.SaveChanges();
+                var cthds = new List<ChiTietHd>();
+                foreach (var item in Cart)
+                {
+                    cthds.Add(new ChiTietHd
+                    {
+                        MaHd = hoadon.MaHd,
+                        SoLuong = item.SoLuong,
+                        DonGia = item.DonGia,
+                        MaHh = item.MaHh,
+                        GiamGia = 0
+                    });
+                }
+                _context.AddRange(cthds);
+                _context.SaveChanges();
+
+                HttpContext.Session.Set<List<CartItem>>(MySetting.CART_KEY, new List<CartItem>());
+
+                return View("Success");
+            }
+            catch
+            {
+                _context.Database.RollbackTransaction();
+            }
+
+            return View(Cart);
         }
     }
 }
